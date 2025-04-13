e270e16db9f458c9c4077802f2c727e682851cfa
null-d-e-v <exception.nulldev@gmail.com>
2025-04-13 18:31:06 -0500

    ✨ (client-utils): Add client utils

---
+++ app/client/shared/auth/login-action-types.ts
@@ -0,0 +1,4 @@
+export const LoginActionType = {
+  REMEMBER: "remember-confirmation",
+  CONFIRM: "valid-confirmation",
+};
---
+++ app/client/shared/page/title.ts
@@ -0,0 +1,3 @@
+export const getPageTitle = ({ title }: { title: string }) => {
+  return `MoodTrack | ${title}`;
+};
